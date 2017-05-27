7309a21c18c2698f3bba3b393afe54b7e51a916e
Maicon Ghidolin <maicon.ghidolin@gmail.com>
2017-05-27 10:20:39 -0300

    Geracao, Determinizacao e Minimizacao

---
+++ Projeto 1 Automato Finito/projeto1 automato finito/projeto1 automato finito/Models/AutomatoFinito.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+
+namespace Models {
+    class AutomatoFinito {
+
+        public List<Estado> estados;
+
+        public AutomatoFinito() {
+            this.estados = new List<Estado>();
+        }
+
+        public string ToString(string separator) {
+
+            return "";
+        }
+
+    }
+}
